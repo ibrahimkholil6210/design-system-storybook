8d2e67a4ffa4f41efe855fd4e83d13e0e7c6d88d
ibrahimkholil6210 <ibrahimkholil550@gmail.com>
2021-06-24 00:51:43 +0600

    Button State

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,23 +6,57 @@ const Button = styled.button`
     font-size: ${typeScale.paragraph};
     cursor: pointer;
     min-width: 100px;
-    font-family: 'Roboto Mono', monospace;
     border-radius: 8px;
     border: none;
+    transition: background-color .3s linear, color .3s linear;
+
+    &:hover{
+        background-color: ${defaultTheme.primaryHoverColor};
+        color: ${defaultTheme.textColorOnPrimary};
+    }
+
+    &:focus{
+        outline: 3px solid ${defaultTheme.primaryActiveColor};
+        outline-offset: 2px;
+    }
+
+    &:active{
+        background-color: ${defaultTheme.primaryActiveColor};
+        border-color: ${defaultTheme.primaryActiveColor};
+        color: ${defaultTheme.textColorOnPrimary}
+    }
 `;
 
 export const PrimaryButton = styled(Button)`
     background-color: ${defaultTheme.primaryColor};
     color: white;
+
+    &:disabled{
+        background: ${defaultTheme.disabled};
+        color: ${defaultTheme.textOnDisabled};
+        cursor: not-allowed;
+    }
 `;
 
 export const SecondaryButton = styled(Button)`
     background-color: white;
     color: ${defaultTheme.primaryColor};
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+    &:disabled{
+        background: none;
+        color: ${defaultTheme.disabled};
+        cursor: not-allowed;
+    }
 `;
 
 export const TertiaryButton = styled(Button)`
     background-color: white;
     color: ${defaultTheme.primaryColor};
+
+    &:disabled{
+        background: none;
+        color: ${defaultTheme.disabled};
+        cursor: not-allowed;
+    }
 `;
--- src/utills/Theme.js
+++ src/utills/Theme.js
@@ -3,10 +3,12 @@ import {primaryFont} from './Typography';
 
 export const defaultTheme = {
     primaryColor: blue[300],
-    primaryColorHover: blue[200],
-    primaryColorActive: blue[100],
+    primaryHoverColor: blue[200],
+    primaryActiveColor: blue[100],
     textColorOnPrimary: neutral[100],
     textColorInverted: neutral[100],
     textColor: neutral[600],
+    disabled: neutral[400],
+    textOnDisabled: neutral[300],
     primaryFont
 }
